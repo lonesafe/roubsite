11961d0740bc6803c51b8b8bacb456cbea000bfa
lonesafe <85195433@qq.com>
2021-12-27 17:01:13 +0800

    更新最新log4j2版本

--- RoubSiteFramework/src/main/java/com/roubsite/web/filter/RSParamCheck.java
+++ RoubSiteFramework/src/main/java/com/roubsite/web/filter/RSParamCheck.java
@@ -141,7 +141,6 @@ public class RSParamCheck {
 	 */
 	private String getIpAddr(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
-		System.out.println("x-forwarded-for ip: " + ip);
 		if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
 			// 多次反向代理后会有多个ip值，第一个ip才是真实ip
 			if (ip.indexOf(",") != -1) {
@@ -150,29 +149,22 @@ public class RSParamCheck {
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
-			System.out.println("Proxy-Client-IP ip: " + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
-			System.out.println("WL-Proxy-Client-IP ip: " + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("HTTP_CLIENT_IP");
-			System.out.println("HTTP_CLIENT_IP ip: " + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-			System.out.println("HTTP_X_FORWARDED_FOR ip: " + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("X-Real-IP");
-			System.out.println("X-Real-IP ip: " + ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
-			System.out.println("getRemoteAddr ip: " + ip);
 		}
-		System.out.println("获取客户端ip: " + ip);
 		return ip;
 	}
 }
